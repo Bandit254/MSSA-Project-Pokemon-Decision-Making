419a8c3e718f19a9908ad41b71e87e1b2c854b07
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-03-11 20:57:56 -0700

    Added the scaffolding for the project's web app (with much more to come)

---
+++ MSSA-CAD-Project-Lugia's-Lair/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSSA_CAD_Project_Lugia_s_Lair.Controllers
+{
+    public class HomeController
+    {
+    }
+}
