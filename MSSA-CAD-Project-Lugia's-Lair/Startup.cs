928c9472e4118d34850018ec8655878438b60ee3
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-03-12 09:03:02 -0700

    Initial Prototype Scaffolding

--- MSSA-CAD-Project-Lugia's-Lair/Startup.cs
+++ MSSA-CAD-Project-Lugia's-Lair/Startup.cs
@@ -16,25 +16,22 @@ namespace MSSA_CAD_Project_Lugia_s_Lair
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
             }
+            app.UseMvcWithDefaultRoute();
+            app.UseStaticFiles();
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
         }
     }
 }
