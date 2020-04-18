765c1c3d9db6af7bc165b916b96f588b8be3a3da
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-04-18 16:17:08 -0700

    Added basic views to display all data for certain models.

---
+++ MSSA-CAD-Project-Lugias-Lair/Controllers/MoveController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MSSA_CAD_Project_Lugias_Lair.Models;
+
+namespace MSSA_CAD_Project_Lugias_Lair.Controllers
+{
+    public class MoveController : Controller
+    {
+        private IPokemonMove repository;
+        public MoveController(IPokemonMove repo)
+        {
+            repository = repo;
+        }
+        public ViewResult DisplayAllMoves()
+        {
+            return View(repository.PokemonMoves);
+        }
+    }
+}
