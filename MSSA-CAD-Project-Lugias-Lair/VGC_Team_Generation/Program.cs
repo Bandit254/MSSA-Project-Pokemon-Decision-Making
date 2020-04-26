bb269b44eaf178a5ead0d6ddace7410a11c79b9e
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-04-26 15:11:33 -0700

    Added a demonstration of the algorithm that will be used to generate all VGC Teams from a given Base Team

---
+++ MSSA-CAD-Project-Lugias-Lair/VGC_Team_Generation/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VGC_Team_Generation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test of algorithm to generate all possible VGC Teams from a given Base Team.\n");
+            Console.WriteLine("Each base team has 6 Pokemon, but a VGC team may contain only 4 Pokemon.\n");
+            Console.WriteLine("Therefore, there should be 15 unique ways to select 4 Pokemon from a base team of 6.\n");
+            string[] baseTeam = new string[] { "Charizard", "Blastoise", "Venusaur", "Snorlax", "Pikachu", "Lapras" };
+            List<string[]> vgcCombinations = GenerateTeams(baseTeam);
+            Console.WriteLine("\n");
+            for (int i = 0; i < vgcCombinations.Count; i++)
+            {
+                Console.WriteLine($"VGC Team #{i + 1}:");
+                foreach (string pokemon in vgcCombinations[i])
+                {
+                    Console.WriteLine($"{pokemon}");
+                }
+                Console.WriteLine("\n");
+            }
+        }
+
+        public static List<string[]> GenerateTeams(string[] baseTeam)
+        {
+            List<string[]> vgcCombinations = new List<string[]>();
+            int x = 0;
+            for (int h = 0; h < baseTeam.Length-3; h++)
+            {
+                for (int i = h+1; i < baseTeam.Length - 2; i++)
+                {
+                    for (int j = i+1; j < baseTeam.Length - 1; j++)
+                    {
+                        for (int k = j+1; k < baseTeam.Length; k++)
+                        {
+                            vgcCombinations.Add(new string[] { baseTeam[h], baseTeam[i], baseTeam[j], baseTeam[k] });
+                            Console.WriteLine($"VGC Team combo #{x+1} has been added to the list");
+                            x++;
+                        }
+                    }
+                }
+            }
+            return vgcCombinations;
+        }
+    }
+}
